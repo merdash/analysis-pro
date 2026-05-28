935005d2be86c3deec64bd5d8ee592dceef4740f
merdash <adammerdash845@gmail.com>
2026-05-28 10:31:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // أضف هذا السطر هنا لتصحيح مسارات الملفات على GitHub Pages
-    base: './', 
-    
+    base: './',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
